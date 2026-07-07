142acdb19da269675e6349546580c028180b087e
PikaMug <2267126+PikaMug@users.noreply.github.com>
2026-07-07 15:41:57 -0400

    Support latest version of Quests

--- src/main/java/org/browsit/mcmmoclassicquests/mcMMOClassicXpGainObjective.java
+++ src/main/java/org/browsit/mcmmoclassicquests/mcMMOClassicXpGainObjective.java
@@ -1,9 +1,22 @@
+/*
+ * Copyright (c) 2026 Browsit, LLC. All rights reserved.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+ * NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.browsit.mcmmoclassicquests;
 
 import com.gmail.nossr50.datatypes.skills.SkillType;
 import com.gmail.nossr50.events.experience.McMMOPlayerXpGainEvent;
 import me.pikamug.quests.enums.ObjectiveType;
 import me.pikamug.quests.module.BukkitCustomObjective;
+import me.pikamug.quests.player.BukkitQuester;
 import me.pikamug.quests.player.Quester;
 import me.pikamug.quests.quests.Quest;
 
@@ -41,7 +54,7 @@ public class mcMMOClassicXpGainObjective extends BukkitCustomObjective {
         if (mcMMOClassicModule.getQuests() == null) {
             return;
         }
-        final Quester quester = mcMMOClassicModule.getQuests().getQuester(event.getPlayer().getUniqueId());
+        final BukkitQuester quester = mcMMOClassicModule.getQuests().getQuester(event.getPlayer().getUniqueId());
         if (quester == null) {
             return;
         }
